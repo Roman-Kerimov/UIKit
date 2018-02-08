5fa68964161cdad91d768ebc740db5099342a284
Roman Kerimov <roma.kerimov@icloud.com>
2018-02-08 03:21:36 +0300

    Initial Commit

---
+++ UIKit/UIKit.h
@@ -0,0 +1,19 @@
+//
+//  UIKit.h
+//  UIKit
+//
+//  Created by Roman Kerimov on 2018-02-08.
+//  Copyright © 2018 Roman Kerimov. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for UIKit.
+FOUNDATION_EXPORT double UIKitVersionNumber;
+
+//! Project version string for UIKit.
+FOUNDATION_EXPORT const unsigned char UIKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UIKit/PublicHeader.h>
+
+
