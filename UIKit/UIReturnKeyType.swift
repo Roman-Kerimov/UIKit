60e4dcf65068ea55e845f359f4badd8af2fecf59
Roman Kerimov <roma.kerimov@icloud.com>
2018-11-02 02:10:03 +0300

    Add UIReturnKeyType

---
+++ UIKit/UIReturnKeyType.swift
@@ -0,0 +1,22 @@
+//
+//  UIReturnKeyType.swift
+//  UIKit
+//
+//  Created by Roman Kerimov on 2018-11-02.
+//  Copyright © 2018 Roman Kerimov. All rights reserved.
+//
+
+public enum UIReturnKeyType: Int {
+    case `default` = 0
+    case go = 1
+    case google = 2
+    case join = 3
+    case next = 4
+    case route = 5
+    case search = 6
+    case send = 7
+    case yahoo = 8
+    case done = 9
+    case emergencyCall = 10
+    case `continue` = 11
+}
